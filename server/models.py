390b81a6bd82d5cf129c4567ebe18bf1ccced6fe
abdulkhaliq anwar <abdulkhaliqanwar@student,moringaschool.com>
2025-04-16 23:57:30 +0300

    Fix extension import for Render deployment

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-from extensions import db
+from server.extensions import db
 from sqlalchemy_serializer import SerializerMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
